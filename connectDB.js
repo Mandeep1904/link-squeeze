166e179120c1581a1ea2a7720dc025cc0b962b87
Mandeep1904 <mandeepgohil19@gmail.com>
2025-03-19 19:14:28 +0530

    refactor: simplify MongoDB connection logic by removing unnecessary options

--- connectDB.js
+++ connectDB.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.CONNECTION_STRING);
         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`❌ Couldn't connect to MongoDB: ${error.message}`);
-        process.exit(1); // Stop the app if DB fails
+        process.exit(1); 
     }
 };
 
